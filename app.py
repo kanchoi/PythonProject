5480d52919d4cd68e5504e6d96f4839704befe95
kanchoi <kchoi6258@gmail.com>
2025-09-28 19:13:04 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+import psycopg2
+
+def get_message():
+    return "Hello World! Field Engineer"
+
+def check_db_connection():
+    conn = psycopg2.connect(
+        dbname=os.getenv("POSTGRES_DB", "testdb"),
+        user=os.getenv("POSTGRES_USER", "testuser"),
+        password=os.getenv("POSTGRES_PASSWORD", "testpassword"),
+        host=os.getenv("POSTGRES_HOST", "localhost"),
+        port= int(os.getenv("POSTGRES_PORT", "5432"))
+    )
+    cur= conn.cursor()
+    cur.execute("SELECT 1")
+    cur.close()
+    conn.close()
+    return result[0] == 1
+
+if __name__ == "__main__":
+    print(get_message())
+    if check_db_connection():
+        print("Database connection established")
